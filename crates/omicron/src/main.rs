9220a7f06ca97b58aff2656a8206b9e28bc76838
ahraman <ahraman.programming@gmail.com>
2025-05-27 13:47:15 +0330

    Pilot main fn for tokio and axum.

--- crates/omicron/src/main.rs
+++ crates/omicron/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    _ = dotenvy::dotenv().unwrap();
+    let server_url = std::env::var("SERVER_URL").unwrap();
+
+    let router = Router::new().route("/", get(root));
+
+    let listener = TcpListener::bind(&server_url).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn root() -> String {
+    "hello, world!".to_string()
 }
