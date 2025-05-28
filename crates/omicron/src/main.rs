920612f35eb106372da5a60d4959ee2edf38722f
ahraman <ahraman.programming@gmail.com>
2025-05-28 19:22:48 +0330

    Restructured main fn to use the new error type instead of unwrapping.

--- crates/omicron/src/main.rs
+++ crates/omicron/src/main.rs
@@ -1,17 +1,23 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    response::{IntoResponse, Response},
+    routing::get,
+};
 use tokio::net::TcpListener;
 
+use omicron::Error;
+
 #[tokio::main]
-async fn main() {
-    _ = dotenvy::dotenv().unwrap();
-    let server_url = std::env::var("SERVER_URL").unwrap();
+async fn main() -> Result<(), Error> {
+    _ = dotenvy::dotenv()?;
+    let server_url = std::env::var("SERVER_URL")?;
 
     let router = Router::new().route("/", get(root));
 
-    let listener = TcpListener::bind(&server_url).await.unwrap();
-    axum::serve(listener, router).await.unwrap();
+    let listener = TcpListener::bind(&server_url).await?;
+    Ok(axum::serve(listener, router).await?)
 }
 
-async fn root() -> String {
-    "hello, world!".to_string()
+async fn root() -> Result<Response, Error> {
+    Ok("hello, world!".into_response())
 }
